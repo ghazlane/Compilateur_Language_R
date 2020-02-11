887e5e58e70b3fbc896eb28ef47e608d99be79b2
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-02-11 22:16:27 +0100

    Analyse syntaxique V 0.1

--- Analyseur_syntaxique.c
+++ Analyseur_syntaxique.c
@@ -34,7 +34,7 @@ void INST(){
                          break;
         case WHILE_TOKEN : TANTQUE();
                             break;
-        case REPEAT_TOKEN : REPEAT_TOKEN();
+        case REPEAT_TOKEN : REPETER();
                             break;
         default : printf("Erreur Anayse syntaxique");
     }
@@ -101,7 +101,194 @@ void FUNCTION(){
     Test_Symbole(ACCF_TOKEN, ERR_CAR_INC);
 }
 
+void ARGUMENT(){
+    if(SYM_COUR.CODE == ID_TOKEN){
+        Test_Symbole(ID_TOKEN, ERR_CAR_INC);
+        ARGUMENT_PRIME();
+    }
+}
 
+void ARGUMENT_PRIME(){
+    switch(SYM_COUR.CODE){
+        case VIR_TOKEN : Test_Symbole(VIR_TOKEN, ERR_CAR_INC);
+                         ARGUMENT();
+                            break;
+        case EGAL_TOKEN : Test_Symbole(EGAL_TOKEN, ERR_CAR_INC);
+                            EXPR();
+                            if(SYM_COUR.CODE == VIR_TOKEN){
+                                while(SYM_COUR.CODE == VIR_TOKEN){
+                                    Test_Symbole(VIR_TOKEN,ERR_CAR_INC );
+                                    ARGUMENT();
+                                }
+                            }
+                            break;
+        default : ;
+    }
+}
+
+void COMMANDE(){
+    switch(SYM_COUR.CODE){
+        case PT_TOKEN : Test_Symbole(PT_TOKEN, ERR_CAR_INC);
+                        Test_Symbole(ID_TOKEN, ERR_CAR_INC);
+                        Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+                        PARAMETRE();
+                        Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+                        break;
+        default :  Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+                        PARAMETRE();
+                        Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+
+    }
+}
+
+void PARAMETRE(){
+    if(SYM_COUR.CODE==ID_TOKEN){
+        Test_Symbole(ID_TOKEN, ERR_CAR_INC);
+        PARAMETRE_PRIME();
+    }
+}
+
+void PARAMETRE_PRIME(){
+    switch(SYM_COUR.CODE){
+        case VIR_TOKEN : while(SYM_COUR.CODE == VIR_TOKEN ){
+                                Test_Symbole(VIR_TOKEN, ERR_CAR_INC);
+                                PARAMETRE();
+                        }
+                        break ;
+        case PT_TOKEN : COMMANDE() ; break;
+        case PO_TOKEN : COMMANDE() ; break;
+        default : ;
+    }
+}
+
+
+void SI(){
+    Test_Symbole(IF_TOKEN, ERR_CAR_INC);
+    Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+    COND();
+    Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+    Test_Symbole(ACCO_TOKEN, ERR_CAR_INC);
+    INSTS();
+    STOP();
+    Test_Symbole(ACCF_TOKEN, ERR_CAR_INC);
+}
+
+void POUR(){
+    Test_Symbole(FOR_TOKEN, ERR_CAR_INC);
+    Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+    Test_Symbole(ID_TOKEN, ERR_CAR_INC);
+    Test_Symbole(IN_TOKEN, ERR_CAR_INC);
+    SEQ();
+    Test_Symbole(PF_TOKEN,  ERR_CAR_INC);
+    Test_Symbole(ACCO_TOKEN, ERR_CAR_INC);
+    INSTS();
+    STOP();
+    Test_Symbole(ACCF_TOKEN, ERR_CAR_INC);
+}
+
+void TANTQUE(){
+    Test_Symbole(WHILE_TOKEN, ERR_CAR_INC);
+    Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+    COND();
+    Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+    Test_Symbole(ACCO_TOKEN, ERR_CAR_INC);
+    INSTS();
+    STOP();
+    Test_Symbole(ACCF_TOKEN, ERR_CAR_INC);
+}
+
+void REPETER(){
+    Test_Symbole(REPEAT_TOKEN,ERR_CAR_INC);
+    Test_Symbole(ACCO_TOKEN, ERR_CAR_INC);
+    INSTS();
+    Test_Symbole(PV_TOKEN, ERR_CAR_INC);
+    Test_Symbole(IF_TOKEN, ERR_CAR_INC);
+    Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+    COND();
+    Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+    Test_Symbole(BREAK_TOKEN,ERR_CAR_INC );
+    Test_Symbole(ACCF_TOKEN, ERR_CAR_INC);
+}
+
+void COND(){
+    EXPR() ;
+    VERF();
+    EXPR();
+}
+
+void EXPR(){
+    TERM();
+    OP();
+    TERM();
+}
+
+void TERM(){
+    switch(SYM_COUR.CODE){
+        case ID_TOKEN : Test_Symbole(ID_TOKEN, ERR_CAR_INC);
+                        break;
+        case NUM_TOKEN : Test_Symbole(NUM_TOKEN, ERR_CAR_INC);
+                            break;
+        default : Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+                  EXPR();
+                  Test_Symbole(PF_TOKEN, ERR_CAR_INC);
+    }
+}
+
+void OP(){
+    switch(SYM_COUR.CODE){
+        case PLUS_TOKEN : Test_Symbole(PLUS_TOKEN, ERR_CAR_INC);
+            break;
+        case MOINS_TOKEN :Test_Symbole(MOINS_TOKEN, ERR_CAR_INC);
+            break;
+        case MULT_TOKEN :Test_Symbole(MULT_TOKEN, ERR_CAR_INC);
+            break;
+        case DIV_TOKEN :Test_Symbole(DIV_TOKEN, ERR_CAR_INC);
+            break;
+    }
+}
+
+void VERF(){
+switch(SYM_COUR.CODE){
+        case DIFF_TOKEN : Test_Symbole(DIFF_TOKEN, ERR_CAR_INC);
+            break;
+        case EGAL_TOKEN :Test_Symbole(EGAL_TOKEN, ERR_CAR_INC);
+            break;
+        case INF_TOKEN :Test_Symbole(INF_TOKEN, ERR_CAR_INC);
+            break;
+        case INFEG_TOKEN :Test_Symbole(INFEG_TOKEN, ERR_CAR_INC);
+            break;
+        case SUP_TOKEN :Test_Symbole(SUP_TOKEN, ERR_CAR_INC);
+            break;
+        case SUPEG_TOKEN :Test_Symbole(SUPEG_TOKEN, ERR_CAR_INC);
+            break;
+    }
+}
+
+void RETURN(){
+    if(SYM_COUR.CODE == RETURN_TOKEN){
+        Test_Symbole(RETURN_TOKEN, ERR_CAR_INC);
+        Test_Symbole(PO_TOKEN, ERR_CAR_INC);
+        INST();
+        Test_Symbole(PF_TOKEN, ERR_CAR_INC)
+    }
+}
+
+void STOP(){
+    switch(SYM_COUR.CODE){
+        case BREAK_TOKEN : Test_Symbole(BREAK_TOKEN, ERR_CAR_INC); break ;
+        case NEXT_TOKEN : Test_Symbole(NEXT_TOKEN, ERR_CAR_INC); break ;
+        default : ;
+    }
+}
+
+void SEQ(){
+    switch(SYM_COUR.CODE){
+        case ID_TOKEN : Test_Symbole(ID_TOKEN, ERR_CAR_INC); break ;
+        case NUM_TOKEN : Test_Symbole(NUM_TOKEN, ERR_CAR_INC);
+                        Test_Symbole(DPT_TOKEN? ERR_CAR_INC);
+                        Test_Symbole(NUM_TOKEN, ERR_CAR_INC);
+    }
+}
 
 
 
